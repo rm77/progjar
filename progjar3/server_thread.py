40dbd1d8057bb7ee49da22e45caa6f0fe865a2fe
Royyana <royyana@gmail.com>
2019-03-20 09:11:49 +0700

    ganti dari localhost ke 0.0.0.0

--- progjar3/server_thread.py
+++ progjar3/server_thread.py
@@ -27,7 +27,7 @@ class Server(threading.Thread):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('localhost',8889))
+		self.my_socket.bind(('0.0.0.0',8889))
 		self.my_socket.listen(1)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
