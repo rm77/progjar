65eaee78360da5c19892255663b8dc1762fa2085
Royyana <royyana@gmail.com>
2019-05-08 08:23:53 +0700

    ok

---
+++ progjar4/chat.py
@@ -0,0 +1,84 @@
+import sys
+import os
+import json
+import uuid
+from queue import Queue
+
+class Chat:
+	def __init__(self):
+		self.sessions={}
+		self.users = {}
+		self.users['messi']={ 'nama': 'Lionel Messi', 'negara': 'Argentina', 'password': 'surabaya', 'incoming' : {}, 'outgoing': {}}
+		self.users['henderson']={ 'nama': 'Jordan Henderson', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
+		self.users['lineker']={ 'nama': 'Gary Lineker', 'negara': 'Inggris', 'password': 'surabaya','incoming': {}, 'outgoing':{}}
+	def proses(self,data):
+		j=data.split(" ")
+		try:
+			command=j[0]
+			if (command=='auth'):
+				username=j[1]
+				password=j[2]
+				return self.autentikasi_user(username,password)
+			else:
+				return {'status': 'ERROR', 'message': 'Protocol Tidak Benar'}
+		except IndexError:
+			return {'status': 'ERROR', 'message': 'Protocol Tidak Benar'}
+	def autentikasi_user(self,username,password):
+		if (username not in self.users):
+			return { 'status': 'ERROR', 'message': 'User Tidak Ada' }
+ 		if (self.users[username]['password']!= password):
+			return { 'status': 'ERROR', 'message': 'Password Salah' }
+		tokenid = str(uuid.uuid4()) 
+		self.sessions[tokenid]=self.users[username]
+		return { 'status': 'OK', 'token': tokenid }
+	def get_user(self,username):
+		if (username not in self.users):
+			return False
+		return self.users[username]
+	def send_message(self,sessionid,username_from,username_dest,message):
+		if (sessionid not in self.sessions):
+			return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+		s_fr = self.get_user(username_from)
+		s_to = self.get_user(username_dest)
+		
+		if (s_fr==False or s_to==False):
+			return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+
+		message = { 'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message }
+		outqueue_sender = s_fr['outgoing']
+		inqueue_receiver = s_to['incoming']
+		try:	
+			outqueue_sender[username_from].put(message)
+		except KeyError:
+			outqueue_sender[username_from]=Queue()
+			outqueue_sender[username_from].put(message)
+		try:
+			inqueue_receiver[username_from].put(message)
+		except KeyError:
+			inqueue_receiver[username_from]=Queue()
+			inqueue_receiver[username_from].put(message)
+		return {'status': 'OK', 'message': 'Message Sent'}
+
+	def get_inbox(self,username):
+		s_fr = self.get_user(username)
+		incoming = s_fr['incoming']
+		msgs={}
+		for users in incoming:
+			msgs[users]=[]
+			while not incoming[users].empty():
+				msgs[users].append(s_fr['incoming'][users].get_nowait())
+			
+		return {'status': 'OK', 'messages': msgs}
+
+
+if __name__=="__main__":
+	j = Chat()
+	print j.proses('auth messi surabaya')
+
+#print j.send_message('xmessi','henderson','hello son')
+#print j.send_message('henderson','messi','hello si')
+#print j.send_message('lineker','messi','hello si dari lineker')
+
+
+#print j.get_inbox('messi')
+#
