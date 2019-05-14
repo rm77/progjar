6973a641e2862b3bef61e32273ca969683c9bdfc
royyana <royyana@gmail.com>
2019-05-14 17:04:50 +0700

    progjar 5, webserver

---
+++ progjar5/server_thread_http.py
@@ -0,0 +1,51 @@
+from socket import *
+import socket
+import threading
+import thread
+import time
+import sys
+import json
+from http import HttpServer
+
+httpserver = HttpServer()
+
+class ProcessTheClient(threading.Thread):
+	def __init__(self,connection,address):
+		self.connection = connection
+		self.address = address
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True:
+			data = self.connection.recv(1024)
+			if data:
+				self.connection.sendall("{}\r\n\r\n" . format(httpserver.proses(data)))
+			else:
+				break
+		self.connection.close()
+
+class Server(threading.Thread):
+	def __init__(self):
+		self.the_clients = []
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		threading.Thread.__init__(self)
+
+	def run(self):
+		self.my_socket.bind(('0.0.0.0',8889))
+		self.my_socket.listen(1)
+		while True:
+			self.connection, self.client_address = self.my_socket.accept()
+			print >> sys.stderr, 'connection from', self.client_address
+			
+			clt = ProcessTheClient(self.connection, self.client_address)
+			clt.start()
+			self.the_clients.append(clt)
+	
+
+def main():
+	svr = Server()
+	svr.start()
+
+if __name__=="__main__":
+	main()
+
