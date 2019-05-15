ac678ce0715582cb5137c3f208ff5a71a5521770
Royyana <royyana@gmail.com>
2019-05-15 10:03:46 +0700

    tambah tugas 5

---
+++ progjar5/perftest.sh
@@ -0,0 +1,5 @@
+#ab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testingab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testing.txt
+
+
+ab -n 100 -c 50 http://localhost:8887/testing.txt
+
