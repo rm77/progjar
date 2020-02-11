ca18c4ef112909d75c8bbbb67b95551161360b21
royyana <royyana@gmail.com>
2020-02-11 10:50:57 +0700

    ok

--- progjar1/client.py
+++ progjar1/client.py
@@ -6,22 +6,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+print(f"connecting to {server_address}")
 sock.connect(server_address)
 
 
 try:
     # Send data
     message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+    print(f"sending {message}")
+    sock.sendall(message.encode())
     # Look for the response
     amount_received = 0
     amount_expected = len(message)
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
+        print(f"{data}")
 finally:
-    print >>sys.stderr, 'closing socket'
+    print("closing")
     sock.close()
--- progjar1/server.py
+++ progjar1/server.py
@@ -3,25 +3,26 @@ import socket
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+server_address = ('127.0.0.1', 10000)
+print(f"starting up on {server_address}")
 sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
 while True:
-    	# Wait for a connection
-    	print >>sys.stderr, 'waiting for a connection'
-    	connection, client_address = sock.accept()
-    	print >>sys.stderr, 'connection from', client_address
-    	# Receive the data in small chunks and retransmit it
-    	while True:
-        	data = connection.recv(32)
-        	print >>sys.stderr, 'received "%s"' % data
-            	if data:
-                	print >>sys.stderr, 'sending data back to the client'
-                	connection.sendall('-->'+data)
-            	else:
-                	print >>sys.stderr, 'no more data from', client_address
-                	break
-        # Clean up the connection
-	connection.close()
+    # Wait for a connection
+    print("waiting for a connection")
+    connection, client_address = sock.accept()
+    print(f"connection from {client_address}")
+    # Receive the data in small chunks and retransmit it
+    while True:
+        data = connection.recv(32)
+        print(f"received {data}")
+        if data:
+            print("sending back data")
+            connection.sendall('-->'+data)
+        else:
+            print >>sys.stderr, 'no more data from', client_address
+            print(f"no more data from {client_address}")
+#            break
+    # Clean up the connection
+    connection.close()
