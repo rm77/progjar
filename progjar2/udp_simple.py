c2b1dc5633664d48722cfd2f9c6afa72f68e876d
royyana <royyana@gmail.com>
2020-02-25 11:49:14 +0700

    ok

--- progjar2/udp_simple.py
+++ progjar2/udp_simple.py
@@ -1,8 +1,10 @@
 
 import socket
 
-TARGET_IP = "10.151.62.1"
-TARGET_PORT = 5005
+TARGET_IP = "10.151.50.50"
+TARGET_PORT = 5006
+
+nama='informatika'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes('ABCDEFGHIJKLMNOPQRSTUV'.encode()),(TARGET_IP,TARGET_PORT))
+sock.sendto(bytes(nama.encode()),(TARGET_IP,TARGET_PORT))
