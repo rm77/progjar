2313b2ebe0a86833ac20ce2da723f2a9e6f704c1
hipzi <zahramillah582@gmail.com>
2021-03-25 15:42:45 +0700

    bisa koneksi ke kedua server

--- progjar1/jawaban/client.py
+++ progjar1/jawaban/client.py
@@ -1,29 +1,32 @@
 import sys
 import socket
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ipserver = ['192.168.122.72', '192.168.122.171']
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print(f"connecting to {server_address}")
-sock.connect(server_address)
+for i in range(2):
+	# Create a TCP/IP socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Connect the socket to the port where the server is listening
+	server_address = (ipserver[i], 10000)
+	print(f"connecting to {server_address}")
+	sock.connect(server_address)
 
 
-try:
-    # Send data
-    message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
-    print(f"sending {message}")
-    sock.sendall(message.encode())
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    msg = ''
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        msg += data.decode("utf-8")
-finally:
-    print(f"{msg}")
-    print("closing")
-    sock.close()
+	try:
+		# Send data
+		message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
+		print(f"sending {message}")
+		sock.sendall(message.encode())
+		# Look for the response
+		amount_received = 0
+		amount_expected = len(message)
+		msg = ''
+		while amount_received < amount_expected:
+		    data = sock.recv(16)
+		    amount_received += len(data)
+		    msg += data.decode("utf-8")
+	finally:
+		print(f"{msg}")
+		print("closing")
+		sock.close()
