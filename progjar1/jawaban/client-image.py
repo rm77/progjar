f1c9d0d5f9a4821af99e26139d5628973b6f67bb
hipzi <zahramillah582@gmail.com>
2021-03-25 10:49:33 +0700

    berhasil mengirim dan rename image

---
+++ progjar1/jawaban/client-image.py
@@ -0,0 +1,34 @@
+import sys
+import socket
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print(f"connecting to {server_address}")
+sock.connect(server_address)
+
+
+try:
+    # Send data
+    send_filename = 'sendimage.jpg'
+    recv_filename = 'recvimage.jpg'
+    size_img = 0;
+    with open(send_filename, 'rb') as file:
+        sendfile = file.read() 
+    sock.sendall(sendfile)
+    print(f"sending {send_filename}")
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(sendfile)
+    with sock,open(recv_filename,'wb') as file:
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            if not data:
+                break
+            file.write(data);
+finally:
+    print(f"{recv_filename} done")
+    sock.close()
