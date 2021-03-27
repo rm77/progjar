7db0d38d9da767c9e47e5db3256156d991f5276a
marsellaeve <marsella.eve@gmail.com>
2021-03-27 21:36:32 +0000

    1b image

--- progjar1/tugas1b/client1.py
+++ progjar1/tugas1b/client1.py
@@ -13,28 +13,45 @@ print(f"connecting to {server_address2}")
 sock1.connect(server_address1)
 sock2.connect(server_address2)
 
-
 try:
     # Send data
-    message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
-    print(f"sending {message}")
-    sock1.sendall(message.encode())
-    sock2.sendall(message.encode())
+    message1 = "logoyahoo.png"
+    message2 = "logogoogle.jpg"
+    fname1 = open(message1,'rb')
+    fname2 = open(message2,'rb')
+    img1 = fname1.read()
+    img2 = fname2.read()
+    print(f"sending {message1}")
+    print(f"sending {message2}")
+    sock1.sendall(img1)
+    sock2.sendall(img2)
+
     # Look for the response
     amount_received1 = 0
-    amount_expected1 = len(message)
-    while amount_received1 < amount_expected1:
-        data1 = sock1.recv(16)
-        amount_received1 += len(data1)
-        print(f"{data1}")
+    amount_expected1 = len(img1)
+    msg1 = 'terima1' + message1
+    #fname1b = open(msg1,'wb')
+    #fname2b = open(msg2,'wb')
+    with sock1, open(msg1,'wb') as file:
+        while amount_received1 < amount_expected1:
+            data1 = sock1.recv(16)
+            amount_received1 += len(data1)
+            if not data1:
+                break
+            file.write(data1)
 
     amount_received2 = 0
-    amount_expected2 = len(message)
-    while amount_received2 < amount_expected2:
-        data2 = sock2.recv(16)
-        amount_received2 += len(data2)
-        print(f"{data2}")
+    amount_expected2 = len(img2)
+    msg2 = 'terima2' + message2
+    with sock2, open(msg2,'wb') as file:
+        while amount_received2 < amount_expected2:
+            data2 = sock2.recv(16)
+            amount_received2 += len(data2)
+            if not data2:
+                break
+            file.write(data2)
 finally:
-    print("closing")
+    print(f"received {msg1}")
+    print(f"received {msg2}")
     sock1.close()
     sock2.close()
