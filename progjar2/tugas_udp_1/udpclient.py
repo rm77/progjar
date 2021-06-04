6028f8f2084bfbda61cb76d4d78e22bdc64cb652
Afif Fadhlurrahman <afif.fadhlurrahman1@gmail.com>
2021-06-04 10:41:44 +0700

    Add files via upload

---
+++ progjar2/tugas_udp_1/udpclient.py
@@ -0,0 +1,14 @@
+import socket
+import time
+
+TARGET_IP = "192.168.122.224"
+TARGET_PORT = 5001
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+angka = 0
+while True:
+    angka = angka+1
+    msg = " ini angka {} " . format(angka)
+    print(msg)
+    sock.sendto(msg.encode(), (TARGET_IP, TARGET_PORT))
+    # time.sleep(1)
