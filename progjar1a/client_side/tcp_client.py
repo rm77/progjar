e698b655c50851cd439d5db9f21d5e750c8c55a2
Ubuntu <royyana@gmail.com>
2022-03-16 21:15:08 +0700

    restrukturisasi progjar1a

--- progjar1a/client_side/tcp_client.py
+++ progjar1a/client_side/tcp_client.py
@@ -3,19 +3,44 @@ import socket
 import json
 import logging
 import xmltodict
+import ssl
+import os
 
 server_address = ('127.0.0.1', 12000)
 
-
-def getdatapemain(nomor=0):
-    cmd=f"getdatapemain {nomor}"
-    hasil = send_command(cmd)
-    return hasil
-
-
-def send_command(command_str):
+def make_socket(destination_address='localhost',port=12000):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (destination_address, port)
+    logging.warning(f"connecting to {server_address}")
     sock.connect(server_address)
+    return sock
+
+def make_secure_socket(destination_address='localhost',port=10000):
+    #get it from https://curl.se/docs/caextract.html
+
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.verify_mode=ssl.CERT_OPTIONAL
+    context.load_verify_locations(os.getcwd() + '/domain.crt')
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (destination_address, port)
+    logging.warning(f"connecting to {server_address}")
+    sock.connect(server_address)
+    secure_socket = context.wrap_socket(sock,server_hostname=destination_address)
+    logging.warning(secure_socket.getpeercert())
+    return secure_socket
+
+
+def send_command(command_str,is_secure=False):
+    alamat_server = server_address[0]
+    port_server = server_address[1]
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# gunakan fungsi diatas
+    if is_secure == True:
+        sock = make_secure_socket(alamat_server,port_server)
+    else:
+        sock = make_socket(alamat_server,port_server)
+
     logging.warning(f"connecting to {server_address}")
     try:
         logging.warning(f"sending message ")
@@ -42,6 +67,12 @@ def send_command(command_str):
         logging.warning("error during data receiving")
         return False
 
+
+
+def getdatapemain(nomor=0):
+    cmd=f"getdatapemain {nomor}"
+    hasil = send_command(cmd)
+    return hasil
 
 if __name__=='__main__':
     h = getdatapemain(1)
