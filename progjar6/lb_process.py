7408aaff0f5acc1dc4a2d356e9bcb936fb85e6bd
royyana <royyana@gmail.com>
2023-04-11 04:09:11 +0700

    change how lb process process the socket

--- progjar6/lb_process.py
+++ progjar6/lb_process.py
@@ -10,9 +10,9 @@ from http import HttpServer
 class BackendList:
 	def __init__(self):
 		self.servers=[]
-		self.servers.append(('10.21.84.254',8000))
-		self.servers.append(('10.21.84.254',8001))
-		self.servers.append(('10.21.84.254',8002))
+		self.servers.append(('127.0.0.1',8000))
+		self.servers.append(('127.0.0.1',8001))
+		self.servers.append(('127.0.0.1',8002))
 #		self.servers.append(('127.0.0.1',9005))
 		self.current=0
 	def getserver(self):
@@ -26,22 +26,25 @@ class BackendList:
 
 
 
-def ProcessTheClient(connection,address,backend_sock):
+def ProcessTheClient(connection,address,backend_sock,mode='toupstream'):
 		try:
 			while True:
 				try:
-					datafrom_client = connection.recv(1024)
-					if datafrom_client:
-							backend_sock.sendall(datafrom_client)
-					else:
-							backend_sock.close()
+					if (mode=='toupstream'):
+						datafrom_client = connection.recv(32)
+						if datafrom_client:
+								backend_sock.sendall(datafrom_client)
+						else:
+								backend_sock.close()
+								break
+					elif (mode=='toclient'):
+						datafrom_backend = backend_sock.recv(32)
 
-					datafrom_backend = backend_sock.recv(1024)
-
-					if datafrom_backend:
-							connection.sendall(datafrom_backend)
-					else:
-							connection.close()
+						if datafrom_backend:
+								connection.sendall(datafrom_backend)
+						else:
+								connection.close()
+								break
 
 				except OSError as e:
 					pass
@@ -65,16 +68,24 @@ def Server():
 		while True:
 				connection, client_address = my_socket.accept()
 				backend_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				backend_sock.settimeout(1)
 				backend_address = backend.getserver()
 				logging.warning(f"{client_address} connecting to {backend_address}")
-				backend_sock.connect(backend_address)
+				try:
+					backend_sock.connect(backend_address)
 
-				#logging.warning("connection from {}".format(client_address))
-				p = executor.submit(ProcessTheClient, connection, client_address,backend_sock)
-				the_clients.append(p)
-				#menampilkan jumlah process yang sedang aktif
-				jumlah = ['x' for i in the_clients if i.running()==True]
-				#print(jumlah)
+					#logging.warning("connection from {}".format(client_address))
+					toupstream = executor.submit(ProcessTheClient, connection, client_address,backend_sock,'toupstream')
+					#the_clients.append(toupstream)
+					toclient = executor.submit(ProcessTheClient, connection, client_address,backend_sock,'toclient')
+					#the_clients.append(toclient)
+
+					#menampilkan jumlah process yang sedang aktif
+					jumlah = ['x' for i in the_clients if i.running()==True]
+					#print(jumlah)
+				except Exception as err:
+					logging.error(err)
+					pass
 
 
 
