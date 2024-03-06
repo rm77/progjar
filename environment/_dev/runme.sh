7a39aaf26b9e98cb5e9504f5de07529b014b6b17
roy <royyana@gmail.com>
2024-03-06 07:40:10 +0700

    ok

--- environment/_dev/runme.sh
+++ environment/_dev/runme.sh
@@ -1,3 +1,4 @@
 sudo chmod -R a+rwx /home/jovyan
-sudo apt install -y iproute2 
+
+PASSWORD=$(echo $PASSWORD | sha1sum | cut -d" " -f1)
 start-notebook.sh --NotebookApp.token='' --NotebookApp.password=''  --NotebookApp.ip='0.0.0.0' --Notebook.autoreload=True --NotebookApp.notebook_dir=/home/jovyan/work --allow-root
